9e627e0570316845f569a89c3d6c7fe0848a1646
cayendo-en-la-locura <prmrrtzcrdvgzmnmrtnzdmn@gmail.com>
2025-11-23 13:16:44 -0600

    Guardar la imagen como "imagen_mensaje.png"

--- recronstruir_desdetxt.py
+++ recronstruir_desdetxt.py
@@ -3,10 +3,7 @@ import numpy as np
 import re
 
 # === 1. Leer el archivo ===
-archivo="imagen_rgb.txt"
-#archivo="imagen_rgb_expandida.txt"
-#archivo = "imagen_rgb_suavizada.txt"
-#archivo = "imagen_rgb_suavizada_vecinos.txt"
+archivo = "imagen_rgb.txt"
 
 with open(archivo, "r") as f:
     lineas = f.readlines()
@@ -22,13 +19,18 @@ for linea in lineas:
 # === 3. Convertir a arreglo NumPy ===
 img = np.array(pixeles, dtype=np.uint8)
 
-# 🔁 Convertir RGB → BGR (porque imshow espera BGR)
+# 🔁 Convertir RGB → BGR (porque imshow y imwrite esperan BGR)
 img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
 # === 4. Mostrar información ===
 print(f"✅ Imagen reconstruida con tamaño: {img.shape[1]}x{img.shape[0]}, canales: {img.shape[2]}")
 
-# === 5. Mostrar la imagen ===
+# === 5. Guardar la imagen ===
+nombre_archivo = "imagen_mensaje.png" 
+cv2.imwrite(nombre_archivo, img)
+print(f"Imagen guardada como '{nombre_archivo}'")
+
+# === 6. Mostrar la imagen ===
 cv2.imshow("Imagen reconstruida", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
